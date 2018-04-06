988dc320acc3ec6a7eb2709ee203a4e43959ac98
bryc <linbrek@gmail.com>
2018-04-06 05:52:24 -0400

    Optimize lookup3.js

--- jshash/hashes/lookup3.js
+++ jshash/hashes/lookup3.js
@@ -12,85 +12,49 @@
 function lookup3(k, init = 0, init2 = 0) {
     function rot(x,r) {return ((x<<r) | (x >>> (32 - r)));}
 
+    init >>>=0, init2 >>>= 0; // optimization
     var len = k.length, a, b, c, o = 0;
-    a = b = c = (0xdeadbeef + len + init) >>> 0;
+    a = b = c = 0xdeadbeef + len + init;
     c += init2;
     while (len > 12) {
-        a += k[o]   | k[o+1]<<8 | k[o+2] <<16 | k[o+3] <<24;
-        b += k[o+4] | k[o+5]<<8 | k[o+6] <<16 | k[o+7] <<24;
-        c += k[o+8] | k[o+9]<<8 | k[o+10]<<16 | k[o+11]<<24;
-
-        a -= c; a ^= rot(c, 4);  c = c + b >>> 0;
-        b -= a; b ^= rot(a, 6);  a = a + c >>> 0;
-        c -= b; c ^= rot(b, 8);  b = b + a >>> 0;
-        a -= c; a ^= rot(c, 16); c = c + b >>> 0;
-        b -= a; b ^= rot(a, 19); a = a + c >>> 0;
-        c -= b; c ^= rot(b, 4);  b = b + a >>> 0;
+        a += k[o] | k[o+1] << 8 | k[o+2] << 16 | k[o+ 3] << 24;
+        b += k[o+4] | k[o+5] << 8 | k[o+6] << 16 | k[o+7] << 24;
+        c += k[o+8] | k[o+9] << 8 | k[o+10] << 16 | k[o+11] << 24;
+        
+        a -= c; a ^= rot(c, 4);  c += b;
+        b -= a; b ^= rot(a, 6);  a += c;
+        c -= b; c ^= rot(b, 8);  b += a;
+        a -= c; a ^= rot(c, 16); c += b;
+        b -= a; b ^= rot(a, 19); a += c;
+        c -= b; c ^= rot(b, 4);  b += a;
+        a >>>= 0, b >>>= 0, c >>>= 0; // more optimizing
 
         len -= 12;
         o += 12;
     }
 
-    switch (len) {
-    case 12:
-        a += k[o]   | k[o+1]<<8 | k[o+2] <<16 | k[o+3] <<24;
-        b += k[o+4] | k[o+5]<<8 | k[o+6] <<16 | k[o+7] <<24;
-        c += k[o+8] | k[o+9]<<8 | k[o+10]<<16 | k[o+11]<<24;
-        break;
-    case 11:
-        a += k[o]   | k[o+1]<<8 | k[o+2] <<16 | k[o+3]<<24;
-        b += k[o+4] | k[o+5]<<8 | k[o+6] <<16 | k[o+7]<<24;
-        c += k[o+8] | k[o+9]<<8 | k[o+10]<<16;
-        break;
-    case 10:
-        a += k[o]   | k[o+1]<<8 | k[o+2]<<16 | k[o+3]<<24;
-        b += k[o+4] | k[o+5]<<8 | k[o+6]<<16 | k[o+7]<<24;
-        c += k[o+8] | k[o+9]<<8;
-        break;
-    case 9:
-        a += k[o]   | k[o+1]<<8 | k[o+2]<<16 | k[o+3]<<24;
-        b += k[o+4] | k[o+5]<<8 | k[o+6]<<16 | k[o+7]<<24;
-        c += k[o+8];
-        break;
-    case 8:
-        a += k[o]   | k[o+1]<<8 | k[o+2]<<16 | k[o+3]<<24;
-        b += k[o+4] | k[o+5]<<8 | k[o+6]<<16 | k[o+7]<<24;
-        break;
-    case 7:
-        a += k[o]   | k[o+1]<<8 | k[o+2]<<16 | k[o+3]<<24;
-        b += k[o+4] | k[o+5]<<8 | k[o+6]<<16;
-        break;
-    case 6:
-        a += k[o]   | k[o+1]<<8 | k[o+2]<<16 | k[o+3]<<24;
-        b += k[o+4] | k[o+5]<<8;
-        break;
-    case 5:
-        a += k[o]   | k[o+1]<<8 | k[o+2]<<16 | k[o+3]<<24;
-        b += k[o+4];
-        break;
-    case 4:
-        a += k[o] | k[o+1]<<8 | k[o+2]<<16 | k[o+3]<<24;
-        break;
-    case 3:
-        a += k[o] | k[o+1]<<8 | k[o+2]<<16;
-        break;
-    case 2:
-        a += k[o] | k[o+1]<<8;
-        break;
-    case 1:
-        a += k[o];
-        break;
-    case 0:
-        return [b >>> 0, c >>> 0];
+    if(len > 0) { // mix only if len>0
+        switch (len) {
+            case 12: c += k[o+11] << 24;
+            case 11: c += k[o+10] << 16;
+            case 10: c += k[o+9] << 8;
+            case  9: c += k[o+8];
+            case  8: b += k[o+7] << 24;
+            case  7: b += k[o+6] << 16;
+            case  6: b += k[o+5] << 8;
+            case  5: b += k[o+4];
+            case  4: a += k[o+3] << 24;
+            case  3: a += k[o+2] << 16;
+            case  2: a += k[o+1] << 8;
+            case  1: a += k[o];
+        }
+        c ^= b; c -= rot(b, 14);
+        a ^= c; a -= rot(c, 11);
+        b ^= a; b -= rot(a, 25);
+        c ^= b; c -= rot(b, 16);
+        a ^= c; a -= rot(c, 4);
+        b ^= a; b -= rot(a, 14);
+        c ^= b; c -= rot(b, 24);
     }
-
-    c ^= b; c -= rot(b, 14);
-    a ^= c; a -= rot(c, 11);
-    b ^= a; b -= rot(a, 25);
-    c ^= b; c -= rot(b, 16);
-    a ^= c; a -= rot(c, 4);
-    b ^= a; b -= rot(a, 14);
-    c ^= b; c -= rot(b, 24);
-
     return [b >>> 0, c >>> 0];
 }
