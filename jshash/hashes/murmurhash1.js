be27834f041fa6df8ec3020df1c9c90ff12b5cc7
bryc <linbrek@gmail.com>
2018-04-07 04:22:35 -0400

    murmurhash1 optim

--- jshash/hashes/murmurhash1.js
+++ jshash/hashes/murmurhash1.js
@@ -4,28 +4,23 @@
     32-bit MurmurHash1 implemented by bryc (github.com/bryc)
 */
 
-function MurmurHash(key, seed = 0) {
-    var m = 3332679571, r = 16;
-    var h = seed ^ Math.imul(key.length, m);
+function MurmurHash1(key, seed = 0) {
+    var m = 3332679571, h = Math.imul(key.length, m) ^ seed;
 
-    for(var i = 0, chunk = -4 & key.length; i < chunk; i += 4) {
+    for(var i = 0, b = key.length & -4; i < b; i += 4) {
         h += key[i+3] << 24 | key[i+2] << 16 | key[i+1] << 8 | key[i];
-        h = Math.imul(h, m);
-        h ^= h >>> r;
+        h = Math.imul(h, m); h ^= h >>> 16;
     }
-
-    switch(3 & key.length) {
+    
+    switch(key.length & 3) {
         case 3: h += key[i+2] << 16;
         case 2: h += key[i+1] << 8;
-        case 1: h += key[i],
-                h = Math.imul(h, m);
-                h ^= h >>> r;
+        case 1: h += key[i];
+                h = Math.imul(h, m); h ^= h >>> 16;
     }
 
-    h = Math.imul(h, m);
-    h ^= h >>> 10;
-    h = Math.imul(h, m);
-    h ^= h >>> 17;
+    h = Math.imul(h, m); h ^= h >>> 10;
+    h = Math.imul(h, m); h ^= h >>> 17;
 
     return h >>> 0;
 }
