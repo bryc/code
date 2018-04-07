3338e3242a852216898f63dacb5571fcc7f20774
bryc <linbrek@gmail.com>
2018-04-07 04:38:24 -0400

    murmurhash2 optim

--- jshash/hashes/murmurhash2.js
+++ jshash/hashes/murmurhash2.js
@@ -5,27 +5,23 @@
 */
 
 function MurmurHash2(key, seed = 0) {
-    var m = 1540483477, r = 24;
-    var h = seed ^ key.length;
+    var m = 1540483477, h = key.length ^ seed;
 
-    for(var i = 0, k, chunk = -4 & key.length; i < chunk; i += 4) {
+    for(var i = 0, k, b = key.length & -4; i < b; i += 4) {
         k = key[i+3] << 24 | key[i+2] << 16 | key[i+1] << 8 | key[i];
-        k = Math.imul(k, m);
-        k ^= k >>> r;
-        k = Math.imul(k, m);
-        h = Math.imul(h, m) ^ k;
+        k = Math.imul(k, m); k ^= k >>> 24;
+        h = Math.imul(h, m) ^ Math.imul(k, m);
     }
 
-    switch (3 & key.length) {
-        case 3: h ^= key[i + 2] << 16;
-        case 2: h ^= key[i + 1] << 8;
+    switch (key.length & 3) {
+        case 3: h ^= key[i+2] << 16;
+        case 2: h ^= key[i+1] << 8;
         case 1: h ^= key[i];
                 h = Math.imul(h, m);
     }
 
     h ^= h >>> 13;
-    h = Math.imul(h, m);
-    h ^= h >>> 15;
+    h = Math.imul(h, m); h ^= h >>> 15;
 
     return h >>> 0;
 }
