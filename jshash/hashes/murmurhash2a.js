74d77c825ce9b8bf3a97e78f5239f30ea960d842
bryc <linbrek@gmail.com>
2018-04-07 05:26:00 -0400

    murmurhash2a optim

--- jshash/hashes/murmurhash2a.js
+++ jshash/hashes/murmurhash2a.js
@@ -2,36 +2,32 @@
     MurmurHash2A
     ---------------
     32-bit MurmurHash2A implemented by bryc (github.com/bryc)
-    Alternative version using Merkle–Damgård construction.
+    Alternate version using Merkle–Damgård construction.
 */
 
 function MurmurHash2(key, seed = 0) {
-    var m = 1540483477, r = 24, l = key.length;
-    var h = seed | 0;
+    var l, k, m = 1540483477, h = seed | 0;
 
-    for(var i = 0, k, chunk = -4 & key.length; i < chunk; i += 4) {
+    for(var i = 0, b = key.length & -4; i < b; i += 4) {
         k = key[i+3] << 24 | key[i+2] << 16 | key[i+1] << 8 | key[i];
-        k = Math.imul(k, m);
-        k ^= k >>> r;
+        k = Math.imul(k, m); k ^= k >>> 24;
         h = Math.imul(h, m) ^ Math.imul(k, m);
     }
+
     k = 0;
-    switch (3 & key.length) {
-        case 3: k ^= key[i + 2] << 16;
-        case 2: k ^= key[i + 1] << 8;
+    switch (key.length & 3) {
+        case 3: k ^= key[i+2] << 16;
+        case 2: k ^= key[i+1] << 8;
         case 1: k ^= key[i];
     }
-    k = Math.imul(k, m);
-    k ^= k >>> r;
-    h = Math.imul(h, m) ^ Math.imul(k, m);
 
-    l = Math.imul(l, m);
-    l ^= l >>> r;
+    k = Math.imul(k, m); k ^= k >>> 24;
+    h = Math.imul(h, m) ^ Math.imul(k, m);
+    l = Math.imul(key.length, m); l ^= l >>> 24;
     h = Math.imul(h, m) ^ Math.imul(l, m);
 
     h ^= h >>> 13;
-    h = Math.imul(h, m);
-    h ^= h >>> 15;
+    h = Math.imul(h, m); h ^= h >>> 15;
 
     return h >>> 0;
 }
