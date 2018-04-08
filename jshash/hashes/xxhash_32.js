c851c6a7f6b6b7b747d313aa6494b5a778136f65
bryc <linbrek@gmail.com>
2018-04-07 22:35:21 -0400

    xxhash optim

--- jshash/hashes/xxhash_32.js
+++ jshash/hashes/xxhash_32.js
@@ -5,40 +5,38 @@
 */
 
 function xxHash(key, seed = 0) {
-    function rotl32(x,r) {return (x << r) | (x >>> (32 - r));}
-    function uint32(k,i) {return k[i+3] << 24 | k[i+2] << 16 | k[i+1] << 8 | k[i];}
-
     var p1 = 2654435761, p2 = 2246822519, p3 = 3266489917, p4 = 668265263, p5 = 374761393,
-        v0 = seed + p5, v1 = seed + p1 + p2, v2 = seed + p2, v3 = seed, v4 = seed - p1, i = 0;
+        v0 = seed+p5 | 0, v1 = seed+p1+p2 | 0, v2 = seed+p2 | 0, v3 = seed | 0, v4 = seed-p1 | 0,
+        i = 0;
 
     if(key.length >= 16) {
-        do {
-            v1 += Math.imul(uint32(key, i), p2), i += 4;
-            v1 = Math.imul(rotl32(v1, 13), p1);
-            v2 += Math.imul(uint32(key, i), p2), i += 4;
-            v2 = Math.imul(rotl32(v2, 13), p1);
-            v3 += Math.imul(uint32(key, i), p2), i += 4;
-            v3 = Math.imul(rotl32(v3, 13), p1);
-            v4 += Math.imul(uint32(key, i), p2), i += 4;
-            v4 = Math.imul(rotl32(v4, 13), p1);
-        } while(i <= key.length - 16);
-
-        v0 = rotl32(v1, 1) + rotl32(v2, 7) + rotl32(v3, 12) + rotl32(v4, 18);
+        while(i <= key.length - 16) {
+            v1 += Math.imul(key[i+3]<<24 | key[i+2]<<16 | key[i+1]<<8 | key[i], p2); i += 4;
+            v1 = Math.imul(v1<<13 | v1>>>19, p1);
+            v2 += Math.imul(key[i+3]<<24 | key[i+2]<<16 | key[i+1]<<8 | key[i], p2); i += 4;
+            v2 = Math.imul(v2<<13 | v2>>>19, p1);
+            v3 += Math.imul(key[i+3]<<24 | key[i+2]<<16 | key[i+1]<<8 | key[i], p2); i += 4;
+            v3 = Math.imul(v3<<13 | v3>>>19, p1);
+            v4 += Math.imul(key[i+3]<<24 | key[i+2]<<16 | key[i+1]<<8 | key[i], p2); i += 4;
+            v4 = Math.imul(v4<<13 | v4>>>19, p1);
+        }
+        v0 += (v1<<1 | v1>>>31) + (v2<<7 | v2>>>25) + (v3<<12 | v3>>>20) + (v4<<18 | v4>>>14);
     }
 
     v0 += key.length;
     while(i <= key.length - 4) {
-        v0 += Math.imul(uint32(key, i), p3), i += 4;
-        v0 = Math.imul(rotl32(v0, 17), p4);
+        v0 += Math.imul(key[i+3]<<24 | key[i+2]<<16 | key[i+1]<<8 | key[i], p3); i += 4;
+        v0 = Math.imul(v0<<17 | v0>>>15, p4);
     }
 
     while(i < key.length) {
         v0 += Math.imul(key[i++], p5);
-        v0 = Math.imul(rotl32(v0, 11), p1);
+        v0 = Math.imul(v0<<11 | v0>>>21, p1);
     }
 
-    v0 = Math.imul(v0 ^ v0 >>> 15, p2);
-    v0 = Math.imul(v0 ^ v0 >>> 13, p3);
+    v0 = Math.imul(v0 ^ v0>>>15, p2);
+    v0 = Math.imul(v0 ^ v0>>>13, p3);
+    v0 ^= v0>>>16;
 
-    return (v0 ^ v0 >>> 16) >>> 0;
+    return v0>>>0;
 }
