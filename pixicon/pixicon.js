5f537dbea1771b77065fb952e0bc4c620eae5631
bryc <linbrek@gmail.com>
2018-08-31 03:07:50 -0400

    pixicon: update sfc32

--- pixicon/pixicon.js
+++ pixicon/pixicon.js
@@ -15,13 +15,12 @@ function pixicon(t, scale, seed, pixels) {
     // Pseudorandom number generator.
     function sfc32(a, b, c, d) {
         return function() {
-          a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
-          var t = (a + b) | 0;
+          a |= 0; b |= 0; c |= 0; d |= 0; 
+          var t = (a + b | 0) + d | 0;
+          d = d + 1 | 0;
           a = b ^ b >>> 9;
           b = c + (c << 3) | 0;
-          c = (c << 21 | c >>> 11);
-          d = d + 1 | 0;
-          t = t + d | 0;
+          c = c << 21 | c >>> 11;
           c = c + t | 0;
           return (t >>> 0) / 4294967296;
         }
