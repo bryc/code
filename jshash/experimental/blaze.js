fbcdf92741bab437cf8302fc7a2ce41a59495d77
bryc <linbrek@gmail.com>
2019-01-23 13:23:21 -0500

    Update blaze.js

--- jshash/experimental/blaze.js
+++ jshash/experimental/blaze.js
@@ -31,3 +31,27 @@ function BlazeHash(key, seed = 0) {
     h2 = Math.imul(h2 ^ h2 >>> 16, p3) ^ Math.imul(h1 ^ h1 >>> 13, p4);
     return [h1 >>> 0, h2 >>> 0]; // 53bit: 4294967296 * (2097151 & h2) + (h1>>>0)
 }
+
+// An alternate version that reads 4 bytes at a time instead of 8, probably inefficient. Improved version of old Cyb-Beta 1.
+
+function BlazeHashB(key, seed = 0) {
+    var p1 = 597399067, p2 = 374761393, p3 = 2246822507, p4 = 3266489909;
+    var h1 = 0xcafebabe ^ seed, h2 = 0xdeadbeef ^ seed;
+    for(var k, i = 0, b = key.length & -4; i < b; i += 4) {
+        k = key[i+3] << 24 | key[i+2] << 16 | key[i+1] << 8 | key[i];
+        h1 ^= k; h1 = Math.imul(h1 ^ h1 >>> 16, p1) ^ h2;
+        h2 ^= k; h2 = Math.imul(h2 ^ h2 >>> 24, p2) ^ h1;
+    }
+    k = 0;
+    switch (3 & key.length) {
+        case 3: k ^= key[i+2] << 16;
+        case 2: k ^= key[i+1] << 8;
+        case 1: k ^= key[i];
+        h1 ^= k; h1 = Math.imul(h1 ^ h1 >>> 16, p1) ^ h2;
+        h2 ^= k; h2 = Math.imul(h2 ^ h2 >>> 24, p2) ^ h1;
+    }
+    h1 ^= key.length;
+    h1 = Math.imul(h1 ^ h1 >>> 16, p3) ^ Math.imul(h2 ^ h2 >>> 13, p4);
+    h2 = Math.imul(h2 ^ h2 >>> 16, p3) ^ Math.imul(h1 ^ h1 >>> 13, p4);
+    return [h1 >>> 0, h2 >>> 0]; // 53bit: 4294967296 * (2097151 & h2) + (h1>>>0)
+}
