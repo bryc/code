995c10893fa040ffceddaf1f60ba9342b1ae4872
bryc <1408749+bryc@users.noreply.github.com>
2023-04-07 14:04:32 -0400

    cyrb53a: less correlated multipliers

--- jshash/experimental/cyrb53.js
+++ jshash/experimental/cyrb53.js
@@ -20,22 +20,23 @@ const cyrb53 = function(str, seed = 0) {
 
 /*
     cyrb53a (c) 2023 bryc (github.com/bryc)
-    There's a somewhat noticeable mixing bias in h1 of the old version.
-    Not enough to worry much about, but I decided to try and fix it.
-    I've tweaked the constants and operations, and now it passes
-    strict avalanche criteria, and even does it in fewer operations.
     Public domain. Attribution appreciated.
+    The original cyrb53 has a slight mixing bias in the low bits of h1.
+    This shouldn't be a huge problem, but I want to try to improve it.
+    This new version should have improved avalanche behavior, but
+    it is not quite final, I may still find improvements.
+    So don't expect it to always produce the same output.
 */
 const cyrb53a = function(str, seed = 0) {
     let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
     for(let i = 0, ch; i < str.length; i++) {
         ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 3545902487);
-        h2 = Math.imul(h2 ^ ch,  569420461);
+        h1 = Math.imul(h1 ^ ch, 0x85ebca77);
+        h2 = Math.imul(h2 ^ ch, 0xc2b2ae3d);
     }
-    h1 ^= Math.imul(h1 ^ (h2 >>> 15), 1935289751);
-    h2 ^= Math.imul(h2 ^ (h1 >>> 15), 4082773399);
-    h2 ^= h1 >>> 16; h1 ^= h2 >>> 16;
+    h1 ^= Math.imul(h1 ^ (h2 >>> 15), 0x735a2d97);
+    h2 ^= Math.imul(h2 ^ (h1 >>> 15), 0xcaf649a9);
+    h1 ^= h2 >>> 16; h2 ^= h1 >>> 16;
     return 2097152 * (h2 >>> 0) + (h1 >>> 11);
 };
 
