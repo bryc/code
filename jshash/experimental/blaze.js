29b8e3805087f2dcfd94f9eb598484768a1834fd
bryc <1408749+bryc@users.noreply.github.com>
2023-04-07 10:30:48 -0400

    Update blaze.js

--- jshash/experimental/blaze.js
+++ jshash/experimental/blaze.js
@@ -56,17 +56,24 @@ function BlazeHashB(key, seed = 0) {
     return [h1 >>> 0, h2 >>> 0]; // 53bit: 4294967296 * (2097151 & h2) + (h1>>>0)
 }
 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// This appears to be basically just an old version of cyrb53 with different constants.
+// I updated it to use the better final mixer, so now it passes SAC even with the different constants.
+
 // An alternate version that reads 1 byte at a time. It's slower (though still fast generally speaking) yet simple.
 // The extra mixing in previous functions seem unnecessary due to the multiplication occuring for every byte.
 
 function BlazeHashC(key, seed = 0) {
-    var p1 = 597399067, p2 = 374761393, p3 = 2246822507, p4 = 3266489909;
-    var h1 = 0xcafebabe ^ seed, h2 = 0xdeadbeef ^ seed;
-    for(var i = 0; i < key.length; i++) {
-        h1 = Math.imul(h1 ^ key[i], p1); h2 = Math.imul(h2 ^ key[i], p2);
+    let h1 = 0xcafebabe ^ seed, h2 = 0xdeadbeef ^ seed;
+    for(let i = 0; i < key.length; i++) {
+        h1 = Math.imul(h1 ^ key[i], 597399067);
+        h2 = Math.imul(h2 ^ key[i], 374761393);
     }
-    h1 = Math.imul(h1 ^ h1 >>> 16, p3) ^ Math.imul(h2 ^ h2 >>> 13, p4);
-    h2 = Math.imul(h2 ^ h2 >>> 16, p3) ^ Math.imul(h1 ^ h1 >>> 13, p4);
-    return [h1 >>> 0, h2 >>> 0]; // 53bit: 4294967296 * (2097151 & h2) + (h1>>>0)
+    h1 ^= Math.imul(h1 ^ (h2 >>> 15), 1935289751);
+    h2 ^= Math.imul(h2 ^ (h1 >>> 15), 4082773399);
+    h2 ^= h1 >>> 16; h1 ^= h2 >>> 16;
+    return [h1 >>> 0, h2 >>> 0]; // 53bit: 2097152 * (h2>>>0) + (h1>>>11)
 }
-
