89dc3b4a7857d1309e2822c788bee594442b935a
bryc <1408749+bryc@users.noreply.github.com>
2023-04-09 09:08:46 -0400

    Improve avalanche in cyb_beta2, but collisions are bad

--- jshash/experimental/cyb_beta2.js
+++ jshash/experimental/cyb_beta2.js
@@ -2,32 +2,53 @@
     Cyb-Beta 2 hash (64-bit) (by github.com/bryc)
     -----------------
     This is a WIP, hasn't been fully tested.
-    But it's very fast and has good basic avalanching and collision resistance.
-    
-    
-    TERRIBLE Avalanche behavior! Don't recommend at all.
+
+
+Collision resistance has issues:
+
+console.log(
+    cyb_beta2([42, 50, 103, 117] ).toString(16) ,
+    cyb_beta2([42, 114, 103, 53] ).toString(16)    
+);
+console.log(
+    cyb_beta2([44, 52, 68, 100] ).toString(16) ,
+    cyb_beta2([44, 116, 68, 36] ).toString(16)    
+);
+console.log(
+    cyb_beta2([110, 43, 95, 45] ).toString(16) ,
+    cyb_beta2([110, 107, 95, 109] ).toString(16)    
+);
+console.log(
+    cyb_beta2([37, 50, 56, 108] ).toString(16) ,
+    cyb_beta2([37, 114, 56, 44] ).toString(16)    
+);
+console.log(
+    cyb_beta2([114, 50, 76, 43] ).toString(16) ,
+    cyb_beta2([114, 66, 76, 91] ).toString(16)    
+);
 */
 
 function cyb_beta2(key, seed = 0) {
-    var m1 = 1540483507, m2 = 3432918353, m3 = 433494437, m4 = 370248451;
-    var h1 = seed ^ Math.imul(key.length, m3) + 1;
-    var h2 = seed ^ Math.imul(key.length, m1) + 1;
+    let h1 = Math.imul(key.length ^ seed,  433494437) | 1,
+        h2 = Math.imul(key.length ^ seed, 1540483507) | 1;
 
-    for (var k, i = 0, chunk = -4 & key.length; i < chunk; i += 4) {
+    for (var i = 0, k, chunk = -4 & key.length; i < chunk; i += 4) {
         k = key[i+3] << 24 | key[i+2] << 16 | key[i+1] << 8 | key[i];
-        k ^= k >>> 24;
-        h1 = h2 ^ Math.imul(h1, m1) ^ k;
-        h2 = h1 ^ Math.imul(h2, m3) ^ k;
+        k ^= k >>> 16;
+        h1 ^= Math.imul(h2 ^ k, 1540483507);
+        h2 ^= Math.imul(h1 ^ k, 433494437);
     }
     switch (3 & key.length) {
-        case 3: h1 ^= key[i+2] << 16, h2 ^= key[i+2] << 16;
-        case 2: h1 ^= key[i+1] << 8, h2 ^= key[i+1] << 8;
-        case 1: h1 ^= key[i], h2 ^= key[i];
-                h1 = h2 ^ Math.imul(h1, m2);
-                h2 = h1 ^ Math.imul(h2, m4);
+        case 3: k ^= key[i+2] << 16;
+        case 2: k ^= key[i+1] << 8;
+        case 1: k ^= key[i];
+                k ^= k >>> 16;
+                h1 ^= Math.imul(h2 ^ k, 3432918353);
+                h2 ^= Math.imul(h1 ^ k, 370248451);
     }
-    h1 ^= h2 >>> 18, h1 = Math.imul(h1, m2), h1 ^= h2 >>> 22;
-    h2 ^= h1 >>> 15, h2 = Math.imul(h2, m3), h2 ^= h1 >>> 13;
+    h1 ^= Math.imul(h1 ^ (h2 >>> 15), 0x735a2d97);
+    h2 ^= Math.imul(h2 ^ (h1 >>> 15), 0xcaf649a9);
+    h1 ^= h2 >>> 16; h2 ^= h1 >>> 16;
 
-    return [h1 >>> 0, h2 >>> 0];
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 }
