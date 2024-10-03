a51eb6762a824f0d7d7304aa0c847107d00aa088
bryc <1408749+bryc@users.noreply.github.com>
2024-10-02 21:25:00 -0400

    Update GoodOAAT.js

--- jshash/hashes/GoodOAAT.js
+++ jshash/hashes/GoodOAAT.js
@@ -2,14 +2,35 @@
     GoodOAAT
     ---------------
     Sokolov Yura's GoodOAAT implemented by bryc (github.com/bryc)
-    An interesting hash function which passes SMHasher3.
+    An interesting 32-bit hash function which passes SMHasher3.
     Possibly slower than MurmurHash3 for Blobs, but may be a strong yet simple string hash.
-    Apparently MurmurHash3 no longer passes the latest fork of SMHasher3.
+    Notably however, MurmurHash3 no longer passes the latest fork - SMHasher3.
 
     Author states:
         "now h1 passes all collision checks, so it is suitable for hash-tables with prime numbers"
 
-    This may indicate that h1 and h2 can be used together to produce 53-bit hashes, but more testing needed.
+    This may indicate that h1 and h2 can be used together to produce 53-bit hashes (or 64-bit).
+    Unfortunately, h1 has 41 failures in SMHasher3 (Was the author using the original SMHasher?):
+    
+    Summary for: GoodOAAT
+    Overall result: FAIL            ( 145 / 186 passed)
+    Failures:
+        Sanity              : [Implementation verification]
+        Avalanche           : [3, 4, 5, 6, 7, 8, 9, 10, 12, 16, 20, 64, 128]
+        BIC                 : [3, 8, 11, 15]
+        Sparse              : [20/3, 9/4]
+        Permutation         : [4-bytes [3 low bits; BE], 4-bytes [3 high bits; LE], 4-bytes [3 high+low bits; LE], 4-bytes [3 high+low bits; BE], 4-bytes [0, low bit; BE], 4-bytes [0, high bit; LE], 8-bytes [0, low bit; BE], 8-bytes [0, high bit; LE]]
+        TwoBytes            : [8]
+        PerlinNoise         : [2]
+        Bitflip             : [3, 4, 8]
+        SeedSparse          : [2]
+        Seed                : [2, 3, 6]
+        SeedAvalanche       : [4]
+        SeedBIC             : [3]
+        SeedBitflip         : [3, 4]
+
+    I'm no sure how severe the failures are, but at the very least, I think it is clear that h1 isn't as good as h2. 
+    I would not suggest using it as a 53/64-bit hash until both halves were to pass SMHasher3.
 */
 
 function GoodOAAT(key, seed = 0) {
